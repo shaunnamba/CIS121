2b4cc395484ee8458315f17e589c9c3aa16d6e7e
shaunnamba <or7572qs@go.minnstate.edu>
2025-09-21 15:09:47 -0500

    3 questions

--- week4'2.py/week4.py
+++ week4'2.py/week4.py
@@ -8,6 +8,27 @@ while larger >smaller:
     print(larger)
 print(count)
 '''
+'''
+#question 2
 user_word= input("")
-for letter in range(1,len(user_word),1):
+result_word=""
+for letter in range(1,len(user_word),2):
     print(user_word[letter])
+    result_word+=word[i]
+    '''
+#question 3
+'''
+for number in range(37, 1051):
+    if number%2==0:
+        print(number)
+'''
+word=""
+user_letter=""
+while user_letter !='done':
+    user_letter=input("Enter a letter(or type done): ")
+    if user_letter!="done":
+        word+=user_letter
+print(word)
+
+
+    
